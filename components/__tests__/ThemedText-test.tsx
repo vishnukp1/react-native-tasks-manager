8a7ba7b1c6283e2e6563cb9e664bb1bcf1718119
vishnu KP <vishnuto16@gmail.com>
2024-12-02 15:46:25 +0530

    new commit

--- components/__tests__/ThemedText-test.tsx
+++ components/__tests__/ThemedText-test.tsx
@@ -5,6 +5,5 @@ import { ThemedText } from '../ThemedText';
 
 it(`renders correctly`, () => {
   const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
-
   expect(tree).toMatchSnapshot();
 });
